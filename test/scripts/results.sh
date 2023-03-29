4f3b5242690e89f4f22bff81a384dc5374e0e546
Matthew R Kasun <mkasun@nusak.ca>
2023-03-29 10:28:13 -0400

    Single test (#17)
    * workflow to run a single test
    
    * var names
    
    * passing test name
    
    * remove test binary
    
    * final result determination
    
    * remove wip
    
    * update results.sh for single test

--- test/scripts/results.sh
+++ test/scripts/results.sh
@@ -1,58 +1,90 @@
 #! /bin/bash
-
-grep ERROR /tmp/clean/clean.log >> /tmp/errors.log
-if [ $? -eq 0 ]
+if [ -f /tmp/clean/log ]
 then
-    echo "removing gateways: FAILED" >> /tmp/results.log
+    grep ERROR /tmp/clean/clean.log >> /tmp/errors.log
+    if [ $? -eq 0 ]
+    then
+        echo "removing gateways: FAILED" >> /tmp/results.log
+    else
+        echo "removing gateways: PASSED" >> /tmp/results.log
+    fi
 else
-    echo "removing gateways: PASSED" >> /tmp/results.log
+    echo "removing gateways: NOT RUN" >> /tmp/results.log
 fi
 
-grep ERROR /tmp/ping/ping.log >> /tmp/errors.log
-if [ $? -eq 0 ]
+if [ -f /tmp/ping/ping.log ]
 then
-    echo "initial ping: FAILED" >> /tmp/results.log
+    grep ERROR /tmp/ping/ping.log >> /tmp/errors.log
+    if [ $? -eq 0 ]
+    then
+        echo "initial ping: FAILED" >> /tmp/results.log
+    else
+        echo "initial ping: PASSED" >> /tmp/results.log
+    fi
 else
-    echo "initial ping: PASSED" >> /tmp/results.log
+    echo "initial ping: NOT RUN" >> /tmp/results.log
 fi
 
-grep ERROR /tmp/tests/peerupdate.log >> /tmp/errors.log
-if [ $? -eq 0 ]
+if [ -f /tmp/tests/peerupdate.log ]
 then
-    echo "peerupdate: FAILED" >> /tmp/results.log
+    grep ERROR /tmp/tests/peerupdate.log >> /tmp/errors.log
+    if [ $? -eq 0 ]
+    then
+        echo "peerupdate: FAILED" >> /tmp/results.log
+    else
+        echo "peerupdate: PASSED" >> /tmp/results.log
+    fi
 else
-    echo "peerupdate: PASSED" >> /tmp/results.log
+    echo "peerupdate: NOT RUN" >> /tmp/results.log
 fi
 
-grep ERROR /tmp/tests/ingress.log >> /tmp/errors.log
-if [ $? -eq 0 ]
-then
-    echo "ingress: FAILED" >> /tmp/results.log
+if [ -f /tmp/tests/ingress.log ]
+    grep ERROR /tmp/tests/ingress.log >> /tmp/errors.log
+    if [ $? -eq 0 ]
+    then
+        echo "ingress: FAILED" >> /tmp/results.log
+    else
+        echo "ingress: PASSED" >> /tmp/results.log
+    fi
 else
-    echo "ingress: PASSED" >> /tmp/results.log
+    echo "ingress: NOT RUN" >> /tmp/results.log
 fi
 
-grep ERROR /tmp/tests/egress.log >> /tmp/errors.log
-if [ $? -eq 0 ]
+if [ -f /tmp/test/egress.log ]
 then
-    echo "egress: FAILED" >> /tmp/results.log
+    grep ERROR /tmp/tests/egress.log >> /tmp/errors.log
+    if [ $? -eq 0 ]
+    then
+        echo "egress: FAILED" >> /tmp/results.log
+    else
+        echo "egress: PASSED" >> /tmp/results.log
+    fi
 else
-    echo "egress: PASSED" >> /tmp/results.log
+    echo "egress: NOT RUN" >> /tmp/results.log
 fi
 
-grep ERROR /tmp/tests/relay.log >> /tmp/errors.log
-if [ $? -eq 0 ]
+if [ -f /tmp/tests/relay.log ]
 then
-    echo "relay: FAILED" >> /tmp/results.log
+    grep ERROR /tmp/tests/relay.log >> /tmp/errors.log
+    if [ $? -eq 0 ]
+    then
+        echo "relay: FAILED" >> /tmp/results.log
+    else
+        echo "relay: PASSED" >> /tmp/results.log
+    fi
 else
-    echo "relay: PASSED" >> /tmp/results.log
+    echo "relay: NOT RUN" >> /tmp/results.log
 fi
 
-grep ERROR /tmp/ping2/ping.log >> /tmp/errors.log
-if [ $? -eq 0 ]
+if [ -f /tmp/ping2/ping.log ]
 then
-    echo "final ping: FAILED" >> /tmp/results.log
+    grep ERROR /tmp/ping2/ping.log >> /tmp/errors.log
+    if [ $? -eq 0 ]
+    then
+        echo "final ping: FAILED" >> /tmp/results.log
+    else
+        echo "final ping: PASSED" >> /tmp/results.log
+    fi
 else
-    echo "final ping: PASSED" >> /tmp/results.log
+    echo "final ping: NOT RUN" >> /tmp/results.log
 fi
-
