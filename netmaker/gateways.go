5ac50ce83bedf1afbc4a0de1db2a67e5bd347708
theguy951357 <crispspiceguitar@gmail.com>
2024-08-08 06:30:18 -0400

    comment out unused function for now

--- netmaker/gateways.go
+++ netmaker/gateways.go
@@ -19,14 +19,25 @@ func GetExtClient(m Netclient, ext string) *models.ExtClient {
 
 func ChangeClient(m Netclient, key string, value int) {
 	slog.Info("changing", key, "to", value)
-	url := "/api/nodes/" + m.Node.Network + "/" + m.Node.ID
+	url := "/api/hosts/" + m.Host.ID
 	data := struct {
 		mtu int
 	}{
 		mtu: value,
 	}
-	callapi[models.ApiNode](http.MethodPut, url, data)
+	callapi[models.ApiHost](http.MethodPut, url, data)
 }
+
+// func changeNode(m Netclient, key string, value string) {
+// 	slog.Info("changing", key, "to", value)
+// 	url := "api/nodes/" + m.Node.Network + "/" + m.Node.ID
+// 	data := struct {
+// 		key string
+// 	}{
+// 		key: value,
+// 	}
+// 	callapi[models.ApiNode](http.MethodPut, url, data)
+// }
 
 func CreateExtClient(client Netclient, network string) string {
 	slog.Info("creating ingress on node", "node", client.Node.ID)
